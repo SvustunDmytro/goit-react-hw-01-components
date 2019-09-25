d223742a628d5ffb110a2b37f2809cd0182ba76f
SvisT <svisteckij@gmail.com>
2019-09-25 08:59:54 +0300

    delete buidl from gitignore

---
+++ build/precache-manifest.fd94126058c59a77312848994098bea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d45cee934e919e659945b3be063c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5efc6d400edbd6f46a39",
+    "url": "/static/css/main.4c912fc9.chunk.css"
+  },
+  {
+    "revision": "0ecfad8c0453e8880384",
+    "url": "/static/js/2.9c83da6a.chunk.js"
+  },
+  {
+    "revision": "5efc6d400edbd6f46a39",
+    "url": "/static/js/main.2ff5f6d6.chunk.js"
+  },
+  {
+    "revision": "2b823539d228b251efb6",
+    "url": "/static/js/runtime-main.605854a9.js"
+  }
+]);
