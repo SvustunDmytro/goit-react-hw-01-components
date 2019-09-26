f5c2e273e4cfb66d5ea42119e725428c3a4e77c5
SvisT <svisteckij@gmail.com>
2019-09-26 14:04:56 +0300

    done some fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// eslint-disable-next-line import/extensions
+import App from './Components/App/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
